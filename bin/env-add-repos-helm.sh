5dd847737a331219183cb8a42f8dad5d2b4f64cf
Shane Ramey <shane.ramey@gmail.com>
2017-07-07 11:04:44 -0700

    delete unused bin directory

--- bin/env-add-repos-helm.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-# set up Helm chart repos. FIXME: move to initialization method
-helm init --client-only
-helm repo add charts.downup.us http://charts.downup.us
-helm repo remove local 2> /dev/null
-
-helm repo update
