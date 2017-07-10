4eff1b38635853c6fdc695d6e206339abfabf24f
Shane Ramey <shane.ramey@gmail.com>
2017-07-10 15:34:05 -0700

    setup.py: add Linux 3rd-party installations

--- landscape/setup.py
+++ landscape/setup.py
@@ -19,7 +19,8 @@ def install_program(program_name, program_version, os_platform):
 	INSTALL_TEMPLATES = {
 		'lastpass': {
 			'Darwin': 'which lpass > /dev/null || (brew update && ' + \
-						'brew install lastpass-cli --with-pinentry)'
+						'brew install lastpass-cli --with-pinentry)',
+			'Linux': 'apt-get update && apt-get install openssl libcurl4-openssl-dev libxml2 libssl-dev libxml2-dev pinentry-curses xclip cmake build-essential pkg-config'
 		},
 		'vault': {
 			'Darwin': 'which vault > /dev/null || (curl -LO ' + \
@@ -28,10 +29,22 @@ def install_program(program_name, program_version, os_platform):
 						'vault_{version}_{platform}_amd64.zip && ' + \
 						'unzip -d /usr/local/bin/ ' + \
 						'vault_{version}_{platform}_amd64.zip && ' + \
-						'rm vault_{version}_{platform}_amd64.zip)'
+						'rm vault_{version}_{platform}_amd64.zip)',
+			'Linux': 'which vault > /dev/null || (curl -LO ' + \
+						'https://releases.hashicorp.com/vault/' + \
+						'{version}/' + \
+						'vault_{version}_{platform}_amd64.zip && ' + \
+						'unzip -d /usr/local/bin/ ' + \
+						'vault_{version}_{platform}_amd64.zip && ' + \
+						'rm vault_{version}_{platform}_amd64.zip)',
 		},
 		'kubectl': {
 			'Darwin': 'which kubectl > /dev/null || (curl -LO ' + \
+						'https://storage.googleapis.com/kubernetes-release/' + \
+						'release/v{version}/bin/{platform}/amd64/' + \
+						'kubectl && chmod +x kubectl && ' + \
+						'mv kubectl /usr/local/bin/)',
+			'Linux': 'which kubectl > /dev/null || (curl -LO ' + \
 						'https://storage.googleapis.com/kubernetes-release/' + \
 						'release/v{version}/bin/{platform}/amd64/' + \
 						'kubectl && chmod +x kubectl && ' + \
@@ -39,6 +52,14 @@ def install_program(program_name, program_version, os_platform):
 		},
 		'helm': {
 			'Darwin': 'which helm > /dev/null || (curl -LO ' + \
+						'https://storage.googleapis.com/kubernetes-helm/' + \
+						'helm-v{version}-{platform}-amd64.tar.gz && ' + \
+						'tar zxvf helm-v{version}-{platform}-amd64.tar.gz ' + \
+						'--strip-components=1 {platform}-amd64/helm && ' + \
+						'chmod +x helm && ' + \
+						'mv helm /usr/local/bin/ && ' + \
+						'rm helm-v{version}-{platform}-amd64.tar.gz)',
+			'Linux': 'which helm > /dev/null || (curl -LO ' + \
 						'https://storage.googleapis.com/kubernetes-helm/' + \
 						'helm-v{version}-{platform}-amd64.tar.gz && ' + \
 						'tar zxvf helm-v{version}-{platform}-amd64.tar.gz ' + \
@@ -55,10 +76,25 @@ def install_program(program_name, program_version, os_platform):
 						'tar zxvf ' + \
 						'landscaper-{version}-{platform}-amd64.tar.gz ' + \
 						'landscaper && mv landscaper /usr/local/bin/ && ' + \
+						'rm landscaper-{version}-{platform}-amd64.tar.gz)',
+			'Linux': 'which landscaper > /dev/null || (curl -LO ' + \
+						'https://github.com/Eneco/landscaper/releases/' + \
+						'download/{version}/' + \
+						'landscaper-{version}-{platform}-amd64.tar.gz && ' + \
+						'tar zxvf ' + \
+						'landscaper-{version}-{platform}-amd64.tar.gz ' + \
+						'landscaper && mv landscaper /usr/local/bin/ && ' + \
 						'rm landscaper-{version}-{platform}-amd64.tar.gz)'
 		},
 		'terraform': {
 			'Darwin': 'which terraform > /dev/null || (curl -LO ' + \
+						'https://releases.hashicorp.com/terraform/' + \
+						'{version}/' + \
+						'terraform_{version}_{platform}_amd64.zip && ' + \
+						'unzip -d /usr/local/bin ' + \
+						'terraform_{version}_{platform}_amd64.zip && ' + \
+						'rm terraform_{version}_{platform}_amd64.zip)',
+			'Linux': 'which terraform > /dev/null || (curl -LO ' + \
 						'https://releases.hashicorp.com/terraform/' + \
 						'{version}/' + \
 						'terraform_{version}_{platform}_amd64.zip && ' + \
@@ -71,8 +107,14 @@ def install_program(program_name, program_version, os_platform):
 						'https://storage.googleapis.com/minikube/' + \
 						'releases/v{version}/minikube-{platform}-amd64 && ' + \
 						'chmod +x minikube && ' + \
+						'mv minikube /usr/local/bin/)',
+			'Linux': 'which minikube > /dev/null || (curl -Lo minikube ' + \
+						'https://storage.googleapis.com/minikube/' + \
+						'releases/v{version}/minikube-{platform}-amd64 && ' + \
+						'chmod +x minikube && ' + \
 						'mv minikube /usr/local/bin/)'
 		},
+
 	}
 
 	# install the tools listed in INSTALL_TEMPLATES
